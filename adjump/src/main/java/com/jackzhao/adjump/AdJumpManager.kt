5dc6c65c3c3c88fdbc665a1e5d30cf9f707ec5a3
jack <13218019903@163.com>
2021-09-05 20:06:56 +0800

    update 3rd party lib

--- adjump/src/main/java/com/jackzhao/adjump/AdJumpManager.kt
+++ adjump/src/main/java/com/jackzhao/adjump/AdJumpManager.kt
@@ -2,28 +2,14 @@ package com.jackzhao.adjump
 
 import android.app.Activity
 import android.content.Context
-import android.provider.Settings
-import android.text.TextUtils
-import android.util.Log
 import com.jackzhao.appmanager.PermissionManager
 
 object AdJumpManager {
     private const val TAG = "AdJumpManager"
+    var isEnable = false
     fun isAdJumpEnable(context: Context): Boolean {
-        val result = ArrayList<String>()
-        val mStringColonSplitter = TextUtils.SimpleStringSplitter(':')
-        val settingValue = Settings.Secure.getString(
-            context.applicationContext.contentResolver,
-            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
-        )
-        if (settingValue != null) {
-            mStringColonSplitter.setString(settingValue)
-            while (mStringColonSplitter.hasNext()) {
-                val accessabilityService = mStringColonSplitter.next()
-                result.add(accessabilityService.split("/".toRegex()).toTypedArray()[0])
-            }
-        }
-        return result.contains(context.packageName)
+        isEnable = PermissionManager.checkSelfAccessbility(context)
+        return isEnable
     }
 
     fun gotoAccessiblityConfig(activity: Activity) {
